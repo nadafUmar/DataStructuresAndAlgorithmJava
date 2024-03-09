cd207d685e0aadf53605f3dbf0c49ff5133ff68a
nadafUmar <nadafmulla97@gmail.com>
2024-03-09 21:01:33 +0545

    adding calculation java program

---
+++ multiplication.java
@@ -0,0 +1,8 @@
+public class multiplication {
+    public static int calculateProduct(int a,int b){
+        return a*b;
+    }
+    public static void main(String[] args) {
+        System.out.println("result after multiplication is :"+calculateProduct(5,6));
+    }
+}
---
+++ substraction.java
@@ -0,0 +1,8 @@
+public class substraction {
+    public static int substract(int a,int b){
+        return a-b;
+    }
+    public static void main(String[] args) {
+        System.out.println("result after substraction is :"+substract(5,2));
+    }
+}
